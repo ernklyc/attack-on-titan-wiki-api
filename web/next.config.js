ec7aa818d65c999fff00d46b6041f933d68e9b84
ernklyc <ernklyc@gmail.com>
2025-05-03 03:10:59 +0300

    Update next.config.js to bypass ESLint during build

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  eslint: {
+    // ESLint kontrollerini derleme sırasında atla
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
